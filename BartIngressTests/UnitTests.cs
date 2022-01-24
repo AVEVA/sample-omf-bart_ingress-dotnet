2ecda7ba78202899bae11c84d652ea07aa806ac1
Anton Dahlin <adahlin@osisoft.com>
2022-01-24 16:00:42 +0100

    adh

--- BartIngressTests/UnitTests.cs
+++ BartIngressTests/UnitTests.cs
@@ -25,18 +25,18 @@ namespace BartIngressTests
             {
                 Program.RunIngress();
 
-                // Wait for data to be processed by OCS
+                // Wait for data to be processed by ADH
                 Thread.Sleep(5000);
 
                 // Edge Data Store and PI Web API process OMF before sending a response, and will return an error code if there is a problem
                 // In this test, the call to RunIngress above will result in an exception if there is a failure on either of those endpoints
 
-                // OCS does not validate OMF before sending a success response, so the test must check that the messages were successful
-                using var ocsAuthenticationHandler = new AuthenticationHandler(Program.Settings.OcsUri, Program.Settings.OcsClientId, Program.Settings.OcsClientSecret);
-                var ocsSdsService = new SdsService(Program.Settings.OcsUri, null, HttpCompressionMethod.GZip, ocsAuthenticationHandler);
-                var ocsDataService = ocsSdsService.GetDataService(Program.Settings.OcsTenantId, Program.Settings.OcsNamespaceId);
-                var ocsValue = ocsDataService.GetLastValueAsync<BartStationEtd>(streamId).Result;
-                Assert.True(ocsValue.TimeStamp > verifyTimestamp);
+                // ADH does not validate OMF before sending a success response, so the test must check that the messages were successful
+                using var adhAuthenticationHandler = new AuthenticationHandler(Program.Settings.AdhUri, Program.Settings.AdhClientId, Program.Settings.AdhClientSecret);
+                var adhSdsService = new SdsService(Program.Settings.AdhUri, null, HttpCompressionMethod.GZip, adhAuthenticationHandler);
+                var adhDataService = adhSdsService.GetDataService(Program.Settings.AdhTenantId, Program.Settings.AdhNamespaceId);
+                var adhValue = adhDataService.GetLastValueAsync<BartStationEtd>(streamId).Result;
+                Assert.True(adhValue.TimeStamp > verifyTimestamp);
             }
             finally
             {
